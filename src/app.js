ffe11ac563fc47c25fc0c176d7ed16301b770f60
Thales Testoni <thalestestoni@gmail.com>
2020-03-31 20:46:38 -0300

    configurated helmet

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 
+import helmet from 'helmet';
 import express from 'express';
 import cors from 'cors';
 import routes from './routes';
@@ -15,6 +16,7 @@ class App {
   }
 
   middlewares() {
+    this.server.use(helmet());
     this.server.use(
       cors({
         origin: process.env.FRONT_URL,
