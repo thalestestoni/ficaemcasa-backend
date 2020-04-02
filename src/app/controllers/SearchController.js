0dcc7e8dab0344c46c6f61a9ae4f1ddc211aede4
Kennedy <kennedymessias.vb@gmail.com>
2020-04-01 23:23:39 -0300

    search controller fixed for user model

--- src/app/controllers/SearchController.js
+++ src/app/controllers/SearchController.js
@@ -1,17 +1,19 @@
-import Necessity from '../models/Necessity';
+import User from '../models/User';
 
 class SearchController {
   async index(request, response) {
     const { latitude, longitude } = request.query;
 
-    const necessities = await Necessity.find({
-      location: {
-        $near: {
-          $geometry: {
-            type: 'Point',
-            coordinates: [longitude, latitude],
+    const necessities = await User.find({
+      necessities: {
+        location: {
+          $near: {
+            $geometry: {
+              type: 'Point',
+              coordinates: [longitude, latitude],
+            },
+            $maxDistance: 10000,
           },
-          $maxDistance: 10000,
         },
       },
     });
