ebb3ad57c0d0843f9a83127e0ef1941b4ef5f3f0
Thales Testoni <thalestestoni@gmail.com>
2020-04-06 19:06:28 -0300

    Restore routes

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,26 @@
 import { Router } from 'express';
 
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 import NecessityController from './app/controllers/NecessityController';
 import SearchController from './app/controllers/SearchController';
 
 const routes = new Router();
 
+routes.post('/user', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.put('/user', UserController.update);
+routes.get('/user/:id', UserController.show);
+routes.delete('/user/', UserController.destroy);
+
 routes.post('/necessity', NecessityController.store);
-routes.get('/necessity', NecessityController.index);
+routes.put('/necessity/:id', NecessityController.update);
+routes.get('/necessity/:id', NecessityController.show);
+routes.get('/necessity/:userId/user', NecessityController.index);
+routes.delete('/necessity/:id', NecessityController.destroy);
 
 routes.get('/search', SearchController.index);
 
