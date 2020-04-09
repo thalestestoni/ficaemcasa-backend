999cfc7c043fa8018fcf1ac9681d54534c14ac68
Thales Testoni <thalestestoni@gmail.com>
2020-04-09 17:25:26 -0300

    Refact NecessityController

--- src/app/controllers/NecessityController.js
+++ src/app/controllers/NecessityController.js
@@ -5,7 +5,8 @@ import Necessity from '../models/Necessity';
 class NecessityController {
   async store(req, res) {
     const schema = Yup.object().shape({
-      necessities: Yup.array().required(),
+      item: Yup.string().required(),
+      category: Yup.string().required(),
       name: Yup.string().required(),
       phone: Yup.string().required(),
       latitude: Yup.number().required(),
@@ -26,13 +27,28 @@ class NecessityController {
 
     const necessity = await Necessity.create(req.body);
 
-    return res.json(necessity);
+    const { _id, item, category, name, phone } = necessity;
+
+    return res.json({
+      _id,
+      item,
+      category,
+      name,
+      phone,
+    });
   }
 
   async show(req, res) {
     const { id } = req.params;
 
-    const necessity = await Necessity.findById(id);
+    const necessity = await Necessity.findById(id, {
+      _id: 0,
+      user_id: 0,
+      location: 0,
+      createdAt: 0,
+      updatedAt: 0,
+      __v: 0,
+    });
 
     if (!necessity) {
       return res.status(400).json({ error: 'Necessity not found' });
@@ -44,7 +60,16 @@ class NecessityController {
   async index(req, res) {
     const { userId } = req.params;
 
-    const necessity = await Necessity.find({ user_id: userId });
+    const necessity = await Necessity.find(
+      { user_id: userId },
+      {
+        user_id: 0,
+        location: 0,
+        createdAt: 0,
+        updatedAt: 0,
+        __v: 0,
+      }
+    );
 
     if (!necessity) {
       return res.status(400).json({ error: 'Necessity or user not found' });
@@ -56,19 +81,49 @@ class NecessityController {
   async update(req, res) {
     const { id } = req.params;
 
-    const necessity = await Necessity.findByIdAndUpdate(id, req.body);
+    const necessity = await Necessity.findById(id);
 
     if (!necessity) {
       return res.status(400).json({ error: 'Necessity not found' });
     }
 
-    return res.json(necessity);
+    if (necessity.user_id !== req.userId) {
+      return res
+        .status(401)
+        .json({ error: "You don't have permission to update this necessity" });
+    }
+
+    await necessity.update(req.body);
+
+    const { item, category, quantity, name, phone } = await Necessity.findById(
+      id
+    );
+
+    return res.json({
+      item,
+      category,
+      quantity,
+      name,
+      phone,
+    });
   }
 
   async destroy(req, res) {
     const { id } = req.params;
 
-    await Necessity.findByIdAndDelete(id);
+    const necessity = await Necessity.findById(id);
+
+    if (!necessity) {
+      return res.status(400).json({ error: 'Necessity not found' });
+    }
+
+    if (necessity.user_id !== req.userId) {
+      return res
+        .status(401)
+        .json({ error: "You don't have permission to delete this necessity" });
+    }
+
+    await necessity.remove();
 
     return res.send();
   }
