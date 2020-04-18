f4c8e0443b46b74dae2c2f844e0c9bd8f1738aec
Thales Testoni <thalestestoni@gmail.com>
2020-04-18 13:27:03 -0300

    Configuração Sentry DEBUG

--- src/config/sentry.js
+++ src/config/sentry.js
@@ -1,3 +1,10 @@
 export default {
   dsn: process.env.SENTRY_DSN,
+  beforeSend: (event) => {
+    if (process.env.DEBUG) {
+      console.error(event);
+      return null;
+    }
+    return event;
+  },
 };
