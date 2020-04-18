a140eb3efdfc9728fb40c00f927a2fbab1bdbbd2
Kennedy <kennedymessias.vb@gmail.com>
2020-04-18 14:10:41 -0300

    user model sons avg age and quantity

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -28,12 +28,12 @@ const UserSchema = new mongoose.Schema(
       type: Boolean,
       required: true,
     },
-    sons: {
+    sonsQuantity: {
       type: Number,
       required: false,
     },
     sonsAverageAge: {
-      type: String,
+      type: Number,
       required: false,
     },
     sonsAtHome: {
@@ -58,7 +58,7 @@ const UserSchema = new mongoose.Schema(
     location: {
       type: PointSchema,
       index: '2dsphere',
-      required: true,
+      required: false,
     },
     password: {
       type: String,
