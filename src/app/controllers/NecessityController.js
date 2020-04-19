732d4d0c048d1887a739a555cb7377473818f60c
Thales Testoni <thalestestoni@gmail.com>
2020-04-19 15:23:04 -0300

    Update status necessity from other user

--- src/app/controllers/NecessityController.js
+++ src/app/controllers/NecessityController.js
@@ -90,6 +90,16 @@ class NecessityController {
     }
 
     if (String(necessity.userId) !== req.userId) {
+      const { status } = req.body;
+
+      if (status) {
+        await Necessity.findByIdAndUpdate(id, {
+          status,
+        });
+
+        return res.send();
+      }
+
       return res
         .status(401)
         .json({ error: "You don't have permission to update this necessity" });
