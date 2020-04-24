4154f501b72206be905d7dd66c3df47b9c555e30
Thales Testoni <thalestestoni@gmail.com>
2020-04-24 14:48:10 -0300

    Alterado nome variavel ambiente enviroment

--- src/app.js
+++ src/app.js
@@ -52,7 +52,7 @@ class App {
 
   exceptionHandler() {
     this.server.use(async (err, req, res, next) => {
-      if (process.env.NODE_ENV === 'development') {
+      if (process.env.ENVIROMENT === 'development') {
         const errors = await new Youch(err, req).toJSON();
 
         return res.status(500).json(errors);
