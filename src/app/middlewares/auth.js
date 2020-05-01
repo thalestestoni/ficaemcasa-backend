d8fde27ba7beead9c7da172fe6e152d14854d094
Thales Testoni <thalestestoni@gmail.com>
2020-05-01 16:25:42 -0300

    Updated auth.js

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -4,7 +4,8 @@ import { promisify } from 'util';
 import authConfig from '../../config/auth';
 
 export default async (req, res, next) => {
-  const token = req.signedCookies.token;
+  const { token } = req.signedCookies;
+
   try {
     const decoded = await promisify(jwt.verify)(token, authConfig.secret);
 
